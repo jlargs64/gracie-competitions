c68eb448ba7fab28529af6480eb4ceaa6acaf896
Justin Largo <justinlargo7@gmail.com>
2024-09-17 11:23:01 -0400

    Working on themeing

---
+++ components/navbar.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import Link from 'next/link';
+import { ThemeToggle } from './theme-toggle';
+
+export function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Gracie Competitions
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/register" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Register
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/login" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Login
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <ThemeToggle />
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
