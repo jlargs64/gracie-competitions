ccf21becfde8bc1f4d91078c5243500d51f7add3
Justin Largo <justinlargo7@gmail.com>
2024-09-18 12:00:24 -0400

    Added more information to READ and fixed dark mode again.

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -8,43 +8,16 @@ import { Button } from '@/components/ui/button';
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
-  function toggleTheme(currentTheme?: string) {
-    console.log(theme, currentTheme);
-    if (currentTheme === undefined) {
-      setTheme('system');
+  function toggleTheme() {
+    if (theme === 'light') {
+      setTheme('dark');
     } else {
-      if (theme === 'light') {
-        setTheme('dark');
-      } else if (theme === 'dark') {
-        setTheme('light');
-      } else {
-        setTheme('system');
-      }
+      setTheme('light');
     }
   }
 
   return (
-    // <DropdownMenu>
-    //   <DropdownMenuTrigger asChild>
-    // <Button variant="outline" size="icon">
-    //   <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-    //   <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    //   <span className="sr-only">Toggle theme</span>
-    // </Button>
-    //   </DropdownMenuTrigger>
-    //   <DropdownMenuContent align="end">
-    //     <DropdownMenuItem onClick={() => setTheme('light')}>
-    //       Light
-    //     </DropdownMenuItem>
-    //     <DropdownMenuItem onClick={() => setTheme('dark')}>
-    //       Dark
-    //     </DropdownMenuItem>
-    //     <DropdownMenuItem onClick={() => setTheme('system')}>
-    //       System
-    //     </DropdownMenuItem>
-    //   </DropdownMenuContent>
-    // </DropdownMenu>
-    <Button variant="outline" size="icon" onClick={() => toggleTheme(theme)}>
+    <Button variant="outline" size="icon" onClick={() => toggleTheme()}>
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
